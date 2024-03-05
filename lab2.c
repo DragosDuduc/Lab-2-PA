2c29880b0ccf10cb714ab0034ded46bf41155f42
Dragos Duduc <dragosduduc81@gmail.com>
2024-03-05 15:43:25 +0200

    Forma 2

--- lab2.c
+++ lab2.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int i=0;
+
 int function(int x){
+    if(x<=0)
+        return 0;
 	x=x-2;
-	///x=function(x);
+	i++;
+	x=function(x);
 	return x;
 }
 
 int main(){
-	int a=30, i=0;
-
-	while (a>0){
+	int a=30;
+	while (a>0)
 		a=function(a);
-		i++;
-	}
 	printf("%d", i);
 	return 0;
 }
